55dd13faab8ce0f1d4e92a4545f0ecf65623eb7b
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-08-21 00:05:39 +0900

    chore: update example

--- usages/algolia/src/index.ts
+++ usages/algolia/src/index.ts
@@ -1,11 +1,7 @@
 import "dotenv/config";
 import { PrismaClient, Prisma } from "@prisma/client";
 import algolia, { SearchIndex } from "algoliasearch";
-import {
-  getSearchStringMapping,
-  getNewWhereArg,
-  searchByAlgoliaIndexes,
-} from "@prisma-fts/algolia";
+import { algoliaFTS } from "@prisma-fts/algolia";
 
 const algoliaClient = algolia(
   process.env.ALGOLIA_APPLICATION_ID ?? "",
@@ -20,33 +16,13 @@ const indexes: Record<Prisma.ModelName, Record<string, SearchIndex>> = {
 };
 
 const prisma = new PrismaClient();
-prisma.$use(async (params, next) => {
-  if (
-    params.action.startsWith("find") &&
-    params.args.where &&
-    params.model &&
-    indexes[params.model]
-  ) {
-    params.args.where = getNewWhereArg(
-      params.args.where,
-      await searchByAlgoliaIndexes(
-        indexes[params.model],
-        getSearchStringMapping(
-          Object.keys(indexes[params.model]),
-          params.args.where
-        ),
-        true
-      )
-    );
-    return next(params);
-  }
-
-  return next(params);
-});
+prisma.$use(algoliaFTS(indexes));
 
 const main = async () => {
   const posts = await prisma.post.findMany({
-    where: { AND: [{ content: "fts:銀河" }, { content: "fts:ねこ" }] },
+    where: {
+      AND: [{ content: "fts:銀河" }, { content: "fts:ジョバンニ" }],
+    },
   });
   console.log(posts);
 };
