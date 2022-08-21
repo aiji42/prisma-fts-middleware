0656f41aeb1607cfcfaa3b1b9115fee3e2b17ebb
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-08-21 15:13:13 +0900

    feat: save indexes in Algolia when creating data by Prisma

--- packages/algolia/src/index.ts
+++ packages/algolia/src/index.ts
@@ -3,7 +3,7 @@ import type { Prisma } from "@prisma/client";
 import { getNewWhereArg, getSearchStringMapping } from "./utils";
 
 export const searchByAlgoliaIndexes = async <T extends boolean>(
-  indexes: Record<string, SearchIndex>,
+  indexMapping: Record<string, SearchIndex>,
   mapping: Record<string, string>,
   pkIsNumber?: T
 ): Promise<
@@ -13,7 +13,7 @@ export const searchByAlgoliaIndexes = async <T extends boolean>(
     await Promise.all(
       Object.entries(mapping).map(async ([key, val]) => {
         const [col] = key.match(/[^.]+$/) ?? [];
-        const index = indexes[col];
+        const index = indexMapping[col];
         const res = await index.search(val);
         return [
           key,
@@ -26,29 +26,57 @@ export const searchByAlgoliaIndexes = async <T extends boolean>(
   );
 };
 
+export const saveObjectOnAlgoLia = async (
+  indexMapping: Record<string, SearchIndex>,
+  data: Record<string, unknown>,
+  pk = "id"
+) => {
+  const objectMapping = Object.entries(indexMapping).reduce<
+    Record<string, { index: SearchIndex; object: Record<string, unknown> }>
+  >((res, [column, index]) => {
+    const key = `${index.appId}-${index.indexName}`;
+    if (!data[column]) return res;
+    if (!res[key])
+      return { ...res, [key]: { index, object: { [column]: data[column] } } };
+    return {
+      ...res,
+      [key]: {
+        ...res[key],
+        object: { ...res[key].object, [column]: data[column] },
+      },
+    };
+  }, {});
+  await Promise.all(
+    Object.values(objectMapping).map(({ index, object }) =>
+      index.saveObject({ ...object, objectID: data[pk] })
+    )
+  );
+};
+
 export const algoliaFTS =
   (
-    indexes: Record<Prisma.ModelName, Record<string, SearchIndex>>
+    indexes: Record<Prisma.ModelName, Record<string, SearchIndex>>,
+    pk = "id"
   ): Prisma.Middleware =>
   async (params, next) => {
-    if (
-      params.action.startsWith("find") &&
-      params.args.where &&
-      params.model &&
-      indexes[params.model]
-    ) {
+    if (!params.model || !indexes[params.model]) return next(params);
+    const indexMapping = indexes[params.model];
+
+    if (params.action.startsWith("find") && params.args.where) {
       params.args.where = getNewWhereArg(
         params.args.where,
         await searchByAlgoliaIndexes(
-          indexes[params.model],
-          getSearchStringMapping(
-            Object.keys(indexes[params.model]),
-            params.args.where
-          ),
+          indexMapping,
+          getSearchStringMapping(Object.keys(indexMapping), params.args.where),
           true
-        )
+        ),
+        pk
       );
       return next(params);
+    }
+    if (params.action === "create") {
+      const record = await next(params);
+      await saveObjectOnAlgoLia(indexMapping, record, pk);
     }
 
     return next(params);
--- packages/algolia/test/index.test.ts
+++ packages/algolia/test/index.test.ts
@@ -1,8 +1,8 @@
 import { test, expect, vi } from "vitest";
-import { searchByAlgoliaIndexes } from "../src";
+import { searchByAlgoliaIndexes, saveObjectOnAlgoLia } from "../src";
 import { SearchIndex } from "algoliasearch";
 
-test("searchByAlgoliaIndexes - 1", async () => {
+test("searchByAlgoliaIndexes - singleIndex", async () => {
   const search = vi.fn();
   search.mockReturnValue({ hits: [{ objectID: "A" }, { objectID: "B" }] });
   const res = await searchByAlgoliaIndexes(
@@ -18,7 +18,7 @@ test("searchByAlgoliaIndexes - 1", async () => {
   expect(res).toEqual({ content: ["A", "B"] });
 });
 
-test("searchByAlgoliaIndexes - 2", async () => {
+test("searchByAlgoliaIndexes - multiIndexes", async () => {
   const search1 = vi.fn(),
     search2 = vi.fn();
   search1.mockReturnValue({ hits: [{ objectID: "1" }, { objectID: "2" }] });
@@ -41,4 +41,50 @@ test("searchByAlgoliaIndexes - 2", async () => {
     "AND.0.content": [1, 2],
     "AND.1.title": [3, 4],
   });
+});
+
+test("saveObjectOnAlgoLia", async () => {
+  const saveObject1 = vi.fn(),
+    saveObject2 = vi.fn(),
+    saveObject3 = vi.fn();
+  const index1 = {
+    appId: "app1",
+    indexName: "index1",
+    saveObject: saveObject1,
+  } as unknown as SearchIndex;
+  const index2 = {
+    appId: "app1",
+    indexName: "index2",
+    saveObject: saveObject2,
+  } as unknown as SearchIndex;
+  const index3 = {
+    appId: "app2",
+    indexName: "index1",
+    saveObject: saveObject3,
+  } as unknown as SearchIndex;
+
+  await saveObjectOnAlgoLia(
+    {
+      title: index1,
+      content: index1,
+      text: index2,
+      note: index3,
+    },
+    {
+      code: "A",
+      title: "title",
+      content: "content",
+      text: "text",
+      note: "note",
+      name: "name",
+    },
+    "code"
+  );
+  expect(saveObject1).toBeCalledWith({
+    objectID: "A",
+    title: "title",
+    content: "content",
+  });
+  expect(saveObject2).toBeCalledWith({ objectID: "A", text: "text" });
+  expect(saveObject3).toBeCalledWith({ objectID: "A", note: "note" });
 });
