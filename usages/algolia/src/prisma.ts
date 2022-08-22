4400df6b200d54d082d2fee52836acb7a249db4b
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-08-22 09:56:02 +0900

    chore: update example

--- usages/algolia/src/prisma.ts
+++ usages/algolia/src/prisma.ts
@@ -1,19 +1,21 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Prisma } from "@prisma/client";
 import { algoliaFTS } from "@prisma-fts/algolia";
 import { algoliaClient } from "./algoliaClient";
 
 export const prisma = new PrismaClient();
-prisma.$use(
-  algoliaFTS(
-    {
-      Post: {
+const middleware = algoliaFTS(
+  Prisma.dmmf,
+  {
+    Post: {
+      objectID: "id",
+      indexes: {
         title: algoliaClient.initIndex("post"),
         content: algoliaClient.initIndex("post"),
       },
     },
-    {
-      syncOn: ["create", "update", "delete"],
-      pKeys: { Post: { name: "id", inNumber: true } },
-    }
-  )
+  },
+  {
+    syncOn: ["create", "update", "delete"],
+  }
 );
+prisma.$use(middleware);
